925a6910f0e81571e1d024f9096118541a4e18fc
kiichi <yougrtcap@gmail.com>
2019-11-19 17:23:20 +0900

    保存されている画像を表示する機能

--- app/views.py
+++ app/views.py
@@ -2,9 +2,12 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 
+from .models import Photo
+
 
 def index(request):
-    return render(request, 'app/index.html')
+    photos = Photo.objects.all().order_by('-created_at')
+    return render(request, 'app/index.html', {'photos': photos})
 
 
 def users_detail(request, pk):

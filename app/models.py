269ed62788226391290fd60804df95e165ab9a59
kiichi <yougrtcap@gmail.com>
2019-11-19 14:49:53 +0900

    ImageFieldを使用　Pillowをインストール

--- app/models.py
+++ app/models.py
@@ -1,1 +1,12 @@
 # Create your models here.
+from django.db import models
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=150)
+    comment = models.TextField(blank=True)
+    image = models.ImageField(upload_to='Photos')
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

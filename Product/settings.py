b2aa6c0c4c4f1c6228b8e0fa388ad97194d371ab
kiichi <yougrtcap@gmail.com>
2019-11-20 00:48:29 +0900

    Categoryモデルを作る

--- Product/settings.py
+++ Product/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
-    'django_cleanup'
+    'app',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,17 @@
-# Register your models here.
 from django.contrib import admin
 
-from .models import Photo
+from .models import Category, Photo
 
 
-class PhotoAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
 
 
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user')
+    list_display_links = ('id', 'title')
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Photo, PhotoAdmin)
